6fa79a83ee18cdd3a7810f14d742cea1e811f197
sosuke0713 <bluecat9686@gmail.com>
2020-12-02 11:12:42 +0900

    second commit

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -11,7 +11,7 @@ class PrototypesController < ApplicationController
   end
 
   def create
-    @prototype = Prototype.new(prototypes_params)
+    @prototype = Prototype.new
     if @prototype.save
       redirect_to root_path
     else
