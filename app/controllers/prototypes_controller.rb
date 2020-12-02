2582f9309d2e7e042a38e5d3d6dfe97fa121b050
sosuke0713 <bluecat9686@gmail.com>
2020-12-02 16:42:35 +0900

    sixth commit

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,6 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show, ]
+  before_action :move_to_index, except: [:index, :show]
 
 
   def index
@@ -52,5 +53,12 @@ class PrototypesController < ApplicationController
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
+
 end
 
