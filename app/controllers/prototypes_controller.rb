40e190eb6082310e7b586cd26ed3b2fdc88060ed
sosuke0713 <bluecat9686@gmail.com>
2020-12-02 15:56:27 +0900

    fifth commit

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -27,9 +27,6 @@ class PrototypesController < ApplicationController
 
   def edit
     @prototype = Prototype.find(params[:id])
-    unless user_signed_in?
-      redirect_to action: :index
-    end
   end
 
   def update
